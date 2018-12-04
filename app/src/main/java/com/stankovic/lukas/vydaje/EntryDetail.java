b6f99e1a3acdf5fc5666f6bf74a1ddcc1b2be5a5
Lukáš Stankovič <lukasstankovic@gmail.com>
2018-12-04 20:51:00 +0100

    Vytvoření aktivity pro detail výdaje

---
+++ app/src/main/java/com/stankovic/lukas/vydaje/EntryDetail.java
@@ -0,0 +1,13 @@
+package com.stankovic.lukas.vydaje;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EntryDetail extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_detail);
+    }
+}
